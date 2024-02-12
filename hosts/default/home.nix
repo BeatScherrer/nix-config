1cbd7a35195a733781d67c3439d663eca6a5768d
Beat Scherrer <beat.scherrer@gmail.com>
2024-02-12 21:49:37 +0100

    setup git with home manager

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -79,4 +79,47 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.git = {
+      enable = true;
+      userName = "BeatScherrer";
+      userEmail = "beat.scherrer@gmail.com";
+      aliases = {
+        co = "checkout";
+        ci = "commit";
+        st = "status -s";
+        br = "branch";
+        pl = "pull";
+        pa = "pull --recurse-submodules";
+        fa = "fetch --all";
+        cs = "clone --recursive -b";
+        lg = "log -20 --oneline --abbrev-commit --pretty=format:\"%h %ad | %s%d [%an]\" --date=short";
+        hist = "log -20 --graph --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short";
+        type = "cat-file -t";
+        dump = "cat-file -p";
+        subup = "submodule update --init --recursive";
+        last = "log -1 HEAD";
+        unstage = "reset HEAD --";
+        mh = "merge -Xignore-space-change -Xrename-threshold=25";
+        unpushed = "log --branches --not --remotes --no-walk --decorate --oneline";
+      };
+      extraConfig = {
+        core = {
+          editor = "vim";
+          hooksPath = "~/.git_hooks";
+        };
+      };
+
+      # merge = {
+      #   tool = "vimdiff";
+      # };
+      # mergetool = {
+      #   prompt = false;
+      #   keepBackup = false;
+      #   vimdiff = {
+      #     cmd = "nvim -d $LOCAL $REMOTE $MERGED -c '$wincmd w' -c 'wincmd J'";
+      #   };
+      # };
+  };
+
 }
