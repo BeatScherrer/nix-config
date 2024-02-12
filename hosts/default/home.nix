17fa2688bc3140b9b0e9cb150e25cbcad2d692ad
BeatScherrer <beat.scherrer@gmail.com>
2024-02-12 22:10:18 +0100

    add alacritty config

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -5,7 +5,7 @@
     #inputs.nix-colors.homeManagerModules.default
     #(modulesPath + "/home-manager/alacritty.nix")
   ];
-  
+
   #colorScheme = inputs.nix-colors.colorScheme.gruvbox-dark-medium;
 
   # Home Manager needs a bit of information about you and the paths it should
@@ -119,5 +119,50 @@
           };
         };
       };
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        size = 12;
+        normal = {
+          family = "JetBrainsMonoNL Nerd Font";
+          style = "Light";
+        };
+      };
+      colors = {
+        primary = {
+          background = "#1d1f21";
+          bright_foreground = "#eaeaea";
+          dim_foreground = "#828482";
+          foreground = "#c5c8c6";
+        };
+        normal = {
+          black = "#282a2e";
+          blue = "#5f819d";
+          cyan = "#5e8d87";
+          green = "#8c9440";
+          magenta = "#85678f";
+          red = "#a54242";
+          white = "#707880";
+          yellow = "#de935f";
+        };
+        bright = {
+          black = "#555b65";
+          blue = "#81a2be";
+          cyan = "#8abeb7";
+          green = "#b5bd68";
+          magenta = "#b294bb";
+          red = "#cc6666";
+          white = "#c5c8c6";
+          yellow = "#ffeac3";
+        };
+        cursor = {
+          cursor = "CellForeground";
+          text = "CellBackground";
+        };
+      };
+    };
   };
 }
