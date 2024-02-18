76656798e42d43bc894cdc67a176d4c941bd775f
BeatScherrer <beat.scherrer@gmail.com>
2024-02-18 11:39:49 +0100

    moved alacritty to its own module

--- home.nix
+++ home.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     inputs.nix-colors.homeManagerModules.default
+    ./modules/alacritty.nix
   ];
 
   colorScheme = inputs.nix-colors.colorSchemes.gruvbox-dark-medium;
@@ -60,19 +61,7 @@
     # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
   # Home Manager can also manage your environment variables through
@@ -112,6 +101,16 @@
   qt.platformTheme = "gtk";
   qt.style.name = "Nordic";
   qt.style.package = pkgs.nordic;
+
+  # TODO: move program configs to program modules to compose a user config conveniently
+ 
+  programs.bash = {
+    shellAliases = {
+      nixupdate = "sudo nixos-rebuild switch --flake ~/.nix";
+      homeupdate = "home-manager switch --flake ~/.nix";
+      nnix = "nvim ~/.nix";
+    };
+  };
 
   programs.git = {
       enable = true;
@@ -155,49 +154,5 @@
   };
 
   # TODO: Use color scheme for alacritty coloring
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      window.decorations = "None";
-      font = {
-        size = 12;
-        normal = {
-          family = "JetBrainsMonoNL Nerd Font";
-          style = "Light";
-        };
-      };
-      colors = with config.colorScheme.colors; {
-        primary = {
-          background = "#1d1f21";
-          bright_foreground = "#eaeaea";
-          dim_foreground = "#828482";
-          foreground = "#c5c8c6";
-        };
-        normal = {
-          black = "#282a2e";
-          blue = "#5f819d";
-          cyan = "#5e8d87";
-          green = "#8c9440";
-          magenta = "#85678f";
-          red = "#a54242";
-          white = "#707880";
-          yellow = "#de935f";
-        };
-        bright = {
-          black = "#555b65";
-          blue = "#81a2be";
-          cyan = "#8abeb7";
-          green = "#b5bd68";
-          magenta = "#b294bb";
-          red = "#cc6666";
-          white = "#c5c8c6";
-          yellow = "#ffeac3";
-        };
-        cursor = {
-          cursor = "CellForeground";
-          text = "CellBackground";
-        };
-      };
-    };
-  };
+
 }
