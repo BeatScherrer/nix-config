940ed865a3db34d5f89793390292d2a64ef0bc88
BeatScherrer <beat.scherrer@gmail.com>
2024-02-19 23:22:04 +0100

    update hlwm

--- modules/herbstluftwm.nix
+++ modules/herbstluftwm.nix
@@ -1,7 +1,15 @@
 { config, pkgs, ...}:
 {
-  home.file = {
-    xdg.configFile."herbstluftwm/autostart".source = ./herbstluftwm/autostart;
-  }
+  xdg.configFile."herbstluftwm/autostart".source = ./herbstluftwm/autostart;
+  xdg.configFile."herbstluftwm/scripts".source = ./herbstluftwm/scripts;
+  xdg.configFile."herbstluftwm/layouts".source = ./herbstluftwm/layouts;
 
+  home.packages = with pkgs; [
+    dconf
+    polybar
+    feh
+    # picom # NOTE: This kills graphics, try nvidia?
+    dunst
+    rofi
+  ];
 }
