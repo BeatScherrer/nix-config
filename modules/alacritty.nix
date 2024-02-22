84faab85fa0ce84c4821ed67cc1091e922cafaf5
BeatScherrer <beat.scherrer@gmail.com>
2024-02-22 22:49:55 +0100

    add colorscheme to alacritty

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -1,5 +1,11 @@
 {config, lib, pkgs, ...}:
+let
+  colorScheme = config.colorScheme.palette;
+in
 {
+  # TODO:
+  # - [ ] add colors from theme
+
   programs.alacritty = {
     enable = true;
     settings = {
@@ -13,31 +19,31 @@
       };
       colors = with config.colorScheme.colors; {
         primary = {
-          background = "#1d1f21";
-          bright_foreground = "#eaeaea";
-          dim_foreground = "#828482";
-          foreground = "#c5c8c6";
+          background = "0x${colorScheme.base00}";
+          bright_foreground = "0x${colorScheme.base07}";
+          dim_foreground = "0x${colorScheme.base04}";
+          foreground = "0x${colorScheme.base06}";
         };
         normal = {
-          black = "#282a2e";
-          blue = "#5f819d";
-          cyan = "#5e8d87";
-          green = "#8c9440";
-          magenta = "#85678f";
-          red = "#a54242";
-          white = "#707880";
-          yellow = "#de935f";
+          black = "0x${colorScheme.base00}";
+          blue = "0x${colorScheme.base0D}";
+          cyan = "0x${colorScheme.base0C}";
+          green = "0x${colorScheme.base0B}";
+          magenta = "0x${colorScheme.base0F}";
+          red = "0x${colorScheme.base08}";
+          white = "0x${colorScheme.base07}";
+          yellow = "0x${colorScheme.base0A}";
         };
-        bright = {
-          black = "#555b65";
-          blue = "#81a2be";
-          cyan = "#8abeb7";
-          green = "#b5bd68";
-          magenta = "#b294bb";
-          red = "#cc6666";
-          white = "#c5c8c6";
-          yellow = "#ffeac3";
-        };
+        # bright = {
+        #   black = "#555b65";
+        #   blue = "#81a2be";
+        #   cyan = "#8abeb7";
+        #   green = "#b5bd68";
+        #   magenta = "#b294bb";
+        #   red = "#cc6666";
+        #   white = "#c5c8c6";
+        #   yellow = "#ffeac3";
+        # };
         cursor = {
           cursor = "CellForeground";
           text = "CellBackground";
