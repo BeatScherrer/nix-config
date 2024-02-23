a48c1803502c7170e84d994f1b3511c7470edae8
BeatScherrer <beat.scherrer@gmail.com>
2024-02-23 23:01:42 +0100

    add gnome tools

--- home.nix
+++ home.nix
@@ -57,6 +57,10 @@
     # node stuff
     # nodePackages.angular;
 
+    # basic gui tools
+    gnome.gnome-calendar
+    gnome-online-accounts
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
