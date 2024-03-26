c8063035a51b006fc48f7bb2688322e554b96b38
BeatScherrer <beat.scherrer@gmail.com>
2024-03-26 10:32:16 +0100

    update get_song script

--- modules/polybar/scripts/get_song.sh
+++ modules/polybar/scripts/get_song.sh
@@ -1,20 +1,30 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-# This script retrieves the information of the song currently played
+getTitle() {
+  local artist
+  local title
+  local output
 
-#ARTIST=$(playerctl metadata artist)
-#TITLE=$(playerctl metadata title)
-#echo "$TITLE - $ARTIST"
+  # if spotify is started
+  if [ "$(pidof spotify)" ]; then
+    # status can be: Playing, Paused or Stopped
+    artist="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null | grep -A 2 -E "artist" | grep -vE "artist" | grep -vE "array" | cut -b 27- | cut -d '"' -f 1 | grep -vE ^$)"
+    title="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null | grep -A 1 -E "title" | grep -vE "title" | cut -b 44- | cut -d '"' -f 1 | grep -vE ^$)"
+    output="$title | $artist"
+  else
+    title="$(playerctl metadata title)"
+    artist="$(playerctl metadata artist)"
+    output="$title | $artist"
+  fi
 
-# if spotify is started
-if [ "$(pidof spotify)" ]; then
-  # status can be: Playing, Paused or Stopped
-  artist=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null | grep -A 2 -E "artist" | grep -vE "artist" | grep -vE "array" | cut -b 27- | cut -d '"' -f 1 | grep -vE ^$)
-  title=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null | grep -A 1 -E "title" | grep -vE "title" | cut -b 44- | cut -d '"' -f 1 | grep -vE ^$)
+  local visible_part="${output:0:20}"
+  local invisible_part="${output:20}"
 
-  echo "$title | $artist"
-else
-  title="$(playerctl metadata title)"
-  artist="$(playerctl metadata artist)"
-  echo "$title | $artist"
-fi
+  if [[ -n "$invisible_part" ]]; then
+    visible_part="${visible_part}..."
+  fi
+
+  echo "$visible_part"
+}
+
+getTitle "$@"
