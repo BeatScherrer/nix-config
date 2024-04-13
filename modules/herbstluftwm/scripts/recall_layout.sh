26dcbf3504db821bcf89607586ba6fa8d7a1d883
Beat Scherrer <beat.scherrer@gmail.com>
2024-04-13 14:31:59 +0200

    fix shebang for nix

--- modules/herbstluftwm/scripts/recall_layout.sh
+++ modules/herbstluftwm/scripts/recall_layout.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -i
+#!/usr/bin/env bash
 
-SELECTION=$(find ~/.config/herbstluftwm/layouts -type f -exec basename {} \; | rofi -dmenu -p "Choose a Layout" -i)
+SELECTION=$(find ~/.config/herbstluftwm/layouts -type f -follow -exec basename {} \; | rofi -dmenu -p "Choose a Layout" -i)
 herbstclient load "$(cat "${HOME}/.config/herbstluftwm/layouts/${SELECTION}")"
