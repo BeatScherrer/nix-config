94474ee1480b3db6fde066a9697d3956c91777fe
BeatScherrer <beat.scherrer@gmail.com>
2024-07-07 16:35:41 +0200

    add zsh custom theme

--- modules/home-manager/shell/zsh/zsh.nix
+++ modules/home-manager/shell/zsh/zsh.nix
@@ -3,6 +3,18 @@ let
   aliases = import ../aliases.nix;
 in
 {
+  # TODO: custom zsh theme (think about this):
+  # - Create a git repo with the custom theme
+  # - use the git repo in nix automatically install the theme on configuration with given commit id
+
+  # copy file to the custom oh-my-zsh directory
+  home.file = {
+    ".zsh_custom/themes" = {
+      source = ./oh-my-zsh/themes;
+      recursive = true;
+    };
+  };
+
   programs = {
     zsh = {
       enable = true;
@@ -11,9 +23,8 @@ in
       shellAliases = aliases;
       oh-my-zsh = {
         enable = true;
-        #custom = "${pkgs.nur.repos....}" # TODO:
-        # theme = "gravel_pit";
-        theme = "robbyrussell";
+        custom = "$HOME/.zsh_custom";
+        theme = "gravel_pit";
         plugins = [
           "git"
           "npm"
