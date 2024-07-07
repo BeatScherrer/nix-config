f848eed068218fd5161fb15d9e37ec50dab01f2d
BeatScherrer <beat.scherrer@gmail.com>
2024-07-07 14:55:24 +0200

    get rid of Oh-my-posh
    setting simple prompt such as container os is not trivially possible and
    therefore there's no point in using it for me.

--- modules/home-manager/shell/shell.nix
+++ modules/home-manager/shell/shell.nix
@@ -5,16 +5,7 @@
     ./zsh/zsh.nix
   ];
 
-  home.packages = with pkgs; [ oh-my-posh ];
-
   programs = {
-    oh-my-posh = {
-      enable = true;
-      # useTheme = "bubblesextra";
-      settings = builtins.fromTOML (
-        builtins.unsafeDiscardStringContext (builtins.readFile ./oh-my-posh/themes/gravel_pit.omp.toml)
-      );
-    };
     fzf = {
       enable = true;
       enableZshIntegration = true;
