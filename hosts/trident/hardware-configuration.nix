74276c502f26fd18714f6b970b2501a4ea068e3f
BeatScherrer <beat.scherrer@gmail.com>
2024-10-19 14:46:45 +0200

    update bluetooth

--- hosts/trident/hardware-configuration.nix
+++ hosts/trident/hardware-configuration.nix
@@ -49,5 +49,18 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = {
+        Name = "Hello";
+        ControllerMode = "dual";
+        FastConnectable = "true";
+        Experimental = "true";
+      };
+      Policy = {
+        AutoEnable = "true";
+      };
+    };
+  };
 }
