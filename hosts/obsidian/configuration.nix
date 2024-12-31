45d88779dd3cefa4855f04d10ed9d6a4e85e3736
BeatScherrer <beat.scherrer@gmail.com>
2024-12-31 16:08:17 +0100

    add home manager apps to quick locate

--- hosts/obsidian/configuration.nix
+++ hosts/obsidian/configuration.nix
@@ -39,6 +39,7 @@
   # `home-manager` currently has issues adding them to `~/Applications`
   # Issue: https://github.com/nix-community/home-manager/issues/1341
   environment.systemPackages = with pkgs; [
+    alacritty
     terminal-notifier
     lazygit
     ripgrep
@@ -61,9 +62,56 @@
   system.keyboard.enableKeyMapping = true;
   system.keyboard.remapCapsLockToEscape = true;
 
+  # allow to move a window by cmd + ctrl + click
   system.defaults.NSGlobalDomain.NSWindowShouldDragOnGesture = true;
+
   # Add ability to used TouchID for sudo authentication
   security.pam.enableSudoTouchIdAuth = true;
+
+  # Nix-darwin does not link installed applications to the user environment. This means apps will not show up
+  # in spotlight, and when launched through the dock they come with a terminal window. This is a workaround.
+  # Upstream issue: https://github.com/LnL7/nix-darwin/issues/214
+  # system.activationScripts.applications.text = lib.mkForce ''
+  #   echo "setting up ~/Applications..." >&2
+  #   applications="$HOME/Applications"
+  #   nix_apps="$applications/Nix Apps"
+  #
+  #   # Needs to be writable by the user so that home-manager can symlink into it
+  #   if ! test -d "$applications"; then
+  #       mkdir -p "$applications"
+  #       chown beat: "$applications"
+  #       chmod u+w "$applications"
+  #   fi
+  #
+  #   # Delete the directory to remove old links
+  #   rm -rf "$nix_apps"
+  #   mkdir -p "$nix_apps"
+  #   find ${config.system.build.applications}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+  #       while read src; do
+  #           # Spotlight does not recognize symlinks, it will ignore directory we link to the applications folder.
+  #           # It does understand MacOS aliases though, a unique filesystem feature. Sadly they cannot be created
+  #           # from bash (as far as I know), so we use the oh-so-great Apple Script instead.
+  #           /usr/bin/osascript -e "
+  #               set fileToAlias to POSIX file \"$src\" 
+  #               set applicationsFolder to POSIX file \"$nix_apps\"
+  #               tell application \"Finder\"
+  #                   make alias file to fileToAlias at applicationsFolder
+  #                   # This renames the alias; 'mpv.app alias' -> 'mpv.app'
+  #                   set name of result to \"$(rev <<< "$src" | cut -d'/' -f1 | rev)\"
+  #               end tell
+  #           " 1>/dev/null
+  #       done
+  # '';
+
+  # home manager spotlight registration
+ system.activationScripts.postUserActivation.text = ''
+   apps_source="$HOME/Applications/Home Manager Apps"
+   moniker="Nix Trampolines"
+   app_target_base="$HOME/Applications"
+   app_target="$app_target_base/$moniker"
+   mkdir -p "$app_target"
+   ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
+ '';
 
   # The value is used to conditionalize
   # backwards‐incompatible changes in default settings. You should
