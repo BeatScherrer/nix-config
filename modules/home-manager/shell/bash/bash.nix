11cf5691a91fcb5f9501453da4638e80d138c5fb
BeatScherrer <beat.scherrer@gmail.com>
2025-01-23 19:41:30 +0100

    update bashrc symlinking

--- modules/home-manager/shell/bash/bash.nix
+++ modules/home-manager/shell/bash/bash.nix
@@ -9,15 +9,21 @@ in
       enable = true;
       shellAliases = aliases;
       bashrcExtra = ''
-        ${bashrc}
+        if [[ -f "$HOME/.bashrcExtra" ]]; then
+          . $HOME/.bashrcExtra
+        fi
+        if [[ -f "$HOME/.bashrc_mt" ]]; then
+          . $HOME/.bashrc_mt
+        fi
       '';
     };
   };
 
+  home.file.".bashrcExtra".source =
+    config.lib.file.mkOutOfStoreSymlink config.home.homeDirectory
+    + "/.nix/modules/home-manager/shell/bash/bashrcExtra";
   home.file.".bashrc_mt".source =
     config.lib.file.mkOutOfStoreSymlink "/home/beat/.nix/modules/home-manager/shell/bash/bashrc_mt";
-  # home.file.".bashrc".source =
-  #   config.lib.file.mkOutOfStoreSymlink "/home/beat/.nix/modules/home-manager/shell/bash/bashrc";
 
   home.file = {
     ".oh-my-bash/custom/themes" = {
