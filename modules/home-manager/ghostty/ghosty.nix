432411f414f992495f80a5b67f6374b3f589bbb0
BeatScherrer <beat.scherrer@gmail.com>
2025-01-27 21:03:42 +0100

    add proper ghostty path

--- modules/home-manager/ghostty/ghosty.nix
+++ modules/home-manager/ghostty/ghosty.nix
@@ -1,4 +1,6 @@
 { config, ... }:
 {
-  home.file.".config/ghostty/config".source = config.lib.file.mkOutOfStoreSymlink ./config;
+  home.file.".config/ghostty/config".source =
+    config.lib.file.mkOutOfStoreSymlink config.home.homeDirectory
+    + "/.nix/modules/home-manager/ghostty/config";
 }
