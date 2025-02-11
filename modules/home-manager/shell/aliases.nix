a68bee092889deddd2e0f192a1a27030d3be1d7f
BeatScherrer <beat.scherrer@gmail.com>
2025-02-11 08:50:16 +0100

    update bash

--- modules/home-manager/shell/aliases.nix
+++ modules/home-manager/shell/aliases.nix
@@ -2,20 +2,9 @@
   nixupdate = "sudo nixos-rebuild switch --flake ~/.nix";
   nixtest = "sudo nixos-rebuild switch --flake ~/.nix";
   homeupdate = "home-manager switch --flake ~/.nix";
-  nnix = "nvim ~/.nix";
-  nnvim = "cd ~/.config/nvim";
-  cdnix = "cd ~/.nix";
-  cdsrc = "cd ~/src";
-  cdbeat = "cdsrc && cd beat";
-  cdmtr = "cdsrc && cd mtrobot";
-  cdschroot = "cdmtr && cd debian-packages/mt-schroot/usr/bin";
-  sourcezsh = "source ~/.zshrc";
   config = "git --git-dir=$HOME/.cfg/ --work-tree=$HOME";
-  cdairshow = "cdsrc && cd beat/airshow";
-  cdaf = "cdsrc && cd beat/albatros_frontend";
   sc = "sudo SYSTEMD_EDITOR=vim systemctl";
   jc = "sudo journalctl";
-  vimf = "nvim $(fzf)";
   cm = "cmake -GNinja";
   mm = "CCACHE_BASEDIR=\${PWD}; echo \"ninja build (CCACHE_BASEDIR set) (MAKE_CORES:\${MAKE_CORES})\"; ninja -j\${MAKE_CORES}";
 }
