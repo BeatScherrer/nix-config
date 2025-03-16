978a0e353cd3ebd6b4c8b49c55adc9c6e82b6f6f
Beat Scherrer <beat.scherrer@gmail.com>
2025-03-16 21:06:24 +0100

    update zellij keybinds and options

--- modules/home-manager/zellij/zellij.nix
+++ modules/home-manager/zellij/zellij.nix
@@ -9,6 +9,42 @@
     enableBashIntegration = false;
     settings = {
       theme = "kanagawa";
+      pane_frames = false;
+      keybinds = {
+        shared = {
+          "bind \"Ctrl h\" \"Ctrl Left\"" = {
+            MoveFocus = "Left";
+          };
+          "bind \"Ctrl j\" \"Ctrl Down\"" = {
+            MoveFocus = "Down";
+          };
+          "bind \"Ctrl k\" \"Ctrl Up\"" = {
+            MoveFocus = "Up";
+          };
+          "bind \"Ctrl l\" \"Ctrl Right\"" = {
+            MoveFocus = "Right";
+          };
+
+          "bind \"Ctrl Shift h\" \"Ctrl Shift Left\"" = {
+            MovePane = "Left";
+          };
+          "bind \"Ctrl Shift j\" \"Ctrl Shift Down\"" = {
+            MovePane = "Down";
+          };
+          "bind \"Ctrl Shift k\" \"Ctrl Shift Up\"" = {
+            MovePane = "Up";
+          };
+          "bind \"Ctrl Shift l\" \"Ctrl Shift Right\"" = {
+            MovePane = "Right";
+          };
+
+          # "bind 'Ctrl h'" = "move-left"; # Move left with Ctrl+H
+          # "bind 'Ctrl l'" = "move-right"; # Move right with Ctrl+L
+        };
+        # shared = {
+        #
+        # };
+      };
     };
   };
 
