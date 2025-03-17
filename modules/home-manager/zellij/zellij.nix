e380a62678415052482fe71add8716d8d97468e8
Beat Scherrer <b.scherrer@mt-robot.com>
2025-03-17 07:54:38 +0100

    add zellij kdl file

--- modules/home-manager/zellij/zellij.nix
+++ modules/home-manager/zellij/zellij.nix
@@ -1,8 +1,7 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    zellij
-  ];
+{ pkgs, ... }: {
+  home.packages = with pkgs; [ zellij ];
+
+  # TODO: use the kdl file so it can be shared more easily to other distros
 
   programs.zellij = {
     enable = true;
@@ -12,28 +11,14 @@
       pane_frames = false;
       keybinds = {
         shared = {
-          "bind \"Ctrl h\" \"Ctrl Left\"" = {
-            MoveFocus = "Left";
-          };
-          "bind \"Ctrl j\" \"Ctrl Down\"" = {
-            MoveFocus = "Down";
-          };
-          "bind \"Ctrl k\" \"Ctrl Up\"" = {
-            MoveFocus = "Up";
-          };
-          "bind \"Ctrl l\" \"Ctrl Right\"" = {
-            MoveFocus = "Right";
-          };
+          "bind \"Ctrl h\" \"Ctrl Left\"" = { MoveFocus = "Left"; };
+          "bind \"Ctrl j\" \"Ctrl Down\"" = { MoveFocus = "Down"; };
+          "bind \"Ctrl k\" \"Ctrl Up\"" = { MoveFocus = "Up"; };
+          "bind \"Ctrl l\" \"Ctrl Right\"" = { MoveFocus = "Right"; };
 
-          "bind \"Ctrl Shift h\" \"Ctrl Shift Left\"" = {
-            MovePane = "Left";
-          };
-          "bind \"Ctrl Shift j\" \"Ctrl Shift Down\"" = {
-            MovePane = "Down";
-          };
-          "bind \"Ctrl Shift k\" \"Ctrl Shift Up\"" = {
-            MovePane = "Up";
-          };
+          "bind \"Ctrl Shift h\" \"Ctrl Shift Left\"" = { MovePane = "Left"; };
+          "bind \"Ctrl Shift j\" \"Ctrl Shift Down\"" = { MovePane = "Down"; };
+          "bind \"Ctrl Shift k\" \"Ctrl Shift Up\"" = { MovePane = "Up"; };
           "bind \"Ctrl Shift l\" \"Ctrl Shift Right\"" = {
             MovePane = "Right";
           };
