c6d551119e454af930adea32277cc049582f7883
Beat Scherrer <beat.scherrer@gmail.com>
2025-04-06 13:51:04 +0200

    update gnome module

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -1,19 +1,29 @@
-{ config, pkgs, ... }:
-{
+{ config, pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     gnome-session
     gnome-tweaks
     gnomeExtensions.pop-shell
     gnome-remote-desktop
+    xorg.xev
+    xorg.xhost
+    xorg.xauth
+    xorg.xeyes
+    xorg.xinit
   ];
 
   services.xserver = {
     xkb.layout = "us";
     xkb.variant = "";
+    xkb.options = "compose:ralt";
     enable = true;
     desktopManager.gnome.enable = true;
     displayManager = {
       gdm.enable = true;
+      # NOTE: another attempt to fix the odyssey g9 monitor issue... this one works
+      sessionCommands = ''
+        ${pkgs.xorg.xset}/bin/xset s off         # Disable screen saver
+        ${pkgs.xorg.xset}/bin/xset -dpms         # Disable DPMS
+      '';
     };
   };
 
