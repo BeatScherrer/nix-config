8b486f597903bba82ea2913ca7819c86bba70c50
Beat Scherrer <beat.scherrer@gmail.com>
2025-04-15 09:57:30 +0200

    fix nixtest alias

--- modules/home-manager/shell/aliases.nix
+++ modules/home-manager/shell/aliases.nix
@@ -1,6 +1,6 @@
 {
   nixupdate = "sudo nixos-rebuild switch --flake ~/.nix";
-  nixtest = "sudo nixos-rebuild switch --flake ~/.nix";
+  nixtest = "sudo nixos-rebuild test --flake ~/.nix";
   homeupdate = "home-manager switch --flake ~/.nix";
   config = "git --git-dir=$HOME/.cfg/ --work-tree=$HOME";
   sc = "sudo SYSTEMD_EDITOR=vim systemctl";
