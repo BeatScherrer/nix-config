ef62d0773184d267a03a38a10a9f24ca8911ac12
Beat Scherrer <beat.scherrer@gmail.com>
2025-04-16 17:12:39 +0200

    only run polybar once on multiple monitors

--- modules/home-manager/polybar/scripts/launch_polybar.sh
+++ modules/home-manager/polybar/scripts/launch_polybar.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
-if command -v xrandr; then
-  for m in $(xrandr --query | grep " connected" | cut -d " " -f 1); do
-    MONITOR=$m polybar --reload main &
-  done
-else
-  polybar --reload main &
-fi
+killall polybar
 
+# if command -v xrandr; then
+#   for m in $(xrandr --query | grep " connected" | cut -d " " -f 1); do
+#     MONITOR=$m polybar --reload main &
+#   done
+# else
+#   polybar --reload main &
+# fi
 
+polybar --reload main &
