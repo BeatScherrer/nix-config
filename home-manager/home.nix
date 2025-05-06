aa403d8e0fce7dc0b8e24828c7a2f8e7c1877d43
Beat Scherrer <beat.scherrer@gmail.com>
2025-05-06 21:09:44 +0000

    add scripts module

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -17,6 +17,7 @@ in
     ../modules/home-manager/avatars/avatars.nix
     ../modules/home-manager/hyprland/hyprland.nix
     ../modules/home-manager/work/mtr.nix
+    ../modules/home-manager/scripts/scripts.nix
     # FIXME:
     # ../modules/home-manager/music_production/music_production.nix
     ../modules/home-manager/synology_drive.nix
--- modules/home-manager/scripts/scripts.nix
+++ modules/home-manager/scripts/scripts.nix
@@ -1,5 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
+let 
+  mkScript = name: file: pkgs.writeShellScriptBin name(builtins.readFile ./scripts/${file});
+in
 {
   # TODO: copy scripts to ~/.local/bin
+  home.packages = [
+    (mkScript "nix-switch-generation" "nix-switch-generation.bash")
 
+  ];
 }
