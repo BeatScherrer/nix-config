18cf2d4e6e332da0f4d84a49f285ed4eb54964ca
Beat Scherrer <beat.scherrer@gmail.com>
2025-05-16 13:52:16 +0200

    more vscode extensions

--- modules/nixos/mtr/vscode.nix
+++ modules/nixos/mtr/vscode.nix
@@ -7,17 +7,42 @@ let
     sha256 = "sha256-AGGioXcK/fjPaFaWk2jqLxovUNR59gwpotcSpGNbj1c=";
   };
   angular-vscode = pkgs.vscode-utils.extensionFromVscodeMarketplace {
-    name = "Angular Language Service";
-    publisher = "ng-template";
+    name = "ng-template";
+    publisher = "Angular";
     version = "20.0.0";
-    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    sha256 = "sha256-87SImzcGbwvf9xtdbD3etqaWe6fMVeCKc+f8qTyFnUA=";
   };
-  cs-dev-kit = pkgs.vscode-utils.extensionFromVscodeMarketplace {
+  csdevkit-vscode = pkgs.vscode-utils.extensionFromVscodeMarketplace {
     name = "csdevkit";
     publisher = "ms-dotnettools";
     version = "1.20.4";
-    sha256 = "sha256-0000000000000000000000000000000000000000000=";
+    sha256 = "sha256-N+p/n427yYzYaIBk3xtoLzItJAE1HsfS0xMQOI3Owa0=";
   };
+  biome-vscode = pkgs.vscode-utils.extensionFromVscodeMarketplace {
+    name = "biome";
+    publisher = "biomejs";
+    version = "2025.5.40952";
+    sha256 = "sha256-km5+KnfNI1QwZ5ao7jaBdvzeiepUHmFa4rMFvmH78aw=";
+  };
+  vim-vscode = pkgs.vscode-utils.extensionFromVscodeMarketplace {
+    name = "vim";
+    publisher = "vscodevim";
+    version = "1.29.1";
+    sha256 = "sha256-KH/NECEAk3u+LjUov1SJx2O9c+566ABadc2S0KY9/I0=";
+  };
+  clangd-vscode = pkgs.vscode-utils.extensionFromVscodeMarketplace {
+    name = "vscode-clangd";
+    publisher = "llvm-vs-code-extensions";
+    version = "0.1.34";
+    sha256 = "sha256-aid2m33HNVa27SUjdUQMxuNSTU38Tbh6m3z/S5Hd83A=";
+  };
+  playwright-vscode = pkgs.vscode-utils.extensionFromVscodeMarketplace {
+    name = "playwright";
+    publisher = "ms-playwright";
+    version = "1.1.14";
+    sha256 = "sha256-OhXpqP5PX/E2125MZQVOz+kdJybh0D6DV202HL5gjO0=";
+  };
+
 in
 {
   environment.systemPackages = with pkgs; [
@@ -25,6 +50,11 @@ in
       vscodeExtensions = [
         angular-vscode
         kanagawa-vscode
+        csdevkit-vscode
+        biome-vscode
+        vim-vscode
+        clangd-vscode
+        playwright-vscode
       ];
     })
   ];
