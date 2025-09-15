61d9619a76c35b1576e70d2c3e13cc67d1027573
Beat Scherrer <beat.scherrer@gmail.com>
2025-09-15 08:59:54 +0200

    update container module
    override the nvidia-container-toolkit package to fix nvidia

--- modules/nixos/container/container.nix
+++ modules/nixos/container/container.nix
@@ -32,12 +32,40 @@ in
   };
 
   config = mkIf cfg.enable {
-    hardware.nvidia-container-toolkit.enable = mkIf cfg.nvidia true;
+    hardware.nvidia-container-toolkit = {
+      enable = mkIf cfg.nvidia true;
+      extraArgs = [
+        "--disable-hook"
+        "create-symlinks"
+      ];
+
+      package = pkgs.nvidia-container-toolkit.overrideAttrs (old: {
+        version = "git";
+        src = pkgs.fetchFromGitHub {
+          owner = "nvidia";
+          repo = "nvidia-container-toolkit";
+          rev = "08b3a388e7b1d447e10d4c4d4a71dca29a98a964"; # v1.18.0-rc.2
+          hash = "sha256-y81UbNoMfIhl9Rf1H3RTRmGR3pysDtKlApLrIxwou9I=";
+        };
+        postPatch = ''
+          substituteInPlace internal/config/config.go \
+            --replace-fail '/usr/bin/nvidia-container-runtime-hook' "$tools/bin/nvidia-container-runtime-hook" \
+            --replace-fail '/sbin/ldconfig' '${pkgs.glibc.bin}/sbin/ldconfig'
+
+          # substituteInPlace tools/container/toolkit/toolkit.go \
+          #   --replace-fail '/sbin/ldconfig' '${pkgs.glibc.bin}/sbin/ldconfig'
+
+          substituteInPlace cmd/nvidia-cdi-hook/update-ldcache/update-ldcache.go \
+            --replace-fail '/sbin/ldconfig' '${pkgs.glibc.bin}/sbin/ldconfig'
+        '';
+      });
+    };
+
     environment.systemPackages =
       with pkgs;
       [ distrobox ]
       ++ optionals cfg.nvidia [
-        nvidia-container-toolkit
+        config.hardware.nvidia-container-toolkit.package
       ];
 
     # enable either one imported module or the other
