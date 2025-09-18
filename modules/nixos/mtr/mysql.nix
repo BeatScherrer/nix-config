f5eb398099440d03837a6e900d57fb1732777142
Beat Scherrer <beat.scherrer@gmail.com>
2025-09-18 14:26:16 +0000

    enable initial mysql setup script

--- modules/nixos/mtr/mysql.nix
+++ modules/nixos/mtr/mysql.nix
@@ -8,16 +8,17 @@
     enable = true;
     package = pkgs.mariadb;
     # NOTE: for mysql workbench root use root password authentication
-    # FIXME:
-    # initialScript = pkgs.writeText "mysql-init" ''
-    #   ALTER USER 'mtr'@'localhost' IDENTIFIED BY '5AWcf.0=IR!1';
-    #
-    #   -- Keep root with unix socket authentication for admin convenience
-    #   -- ALTER USER 'root'@'localhost' IDENTIFIED VIA unix_socket;
-    #   ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
-    #
-    #   FLUSH PRIVILEGES;
-    # '';
+    # WARN: This is only executed on initial setup!
+    initialScript = pkgs.writeText "mysql-init.sql" ''
+      CREATE USER IF NOT EXISTS 'mtr'@'localhost' IDENTIFIED BY '5AWcf.0=IR!1';
+      GRANT ALL PRIVILEGES ON *.* TO 'mtr'@'localhost';
+
+      -- Keep root with unix socket authentication for admin convenience
+      -- ALTER USER 'root'@'localhost' IDENTIFIED VIA unix_socket;
+      ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
+
+      FLUSH PRIVILEGES;
+    '';
     configFile = pkgs.writeText "my.cf" ''
       [mysqld]
       default-time-zone = "+00:00"
