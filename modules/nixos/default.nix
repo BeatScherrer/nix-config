38bc4a3b05aa6c01af0b0d306e3c241c73f9793d
Beat Scherrer <beat.scherrer@gmail.com>
2026-01-04 10:28:11 +0100

    add gst plugins

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -59,6 +59,14 @@ in
     dbus
 
     pkg-config
+
+    # GStreamer plugins for media playback (required by many applications)
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav
   ];
 
   programs.bash.blesh = {
