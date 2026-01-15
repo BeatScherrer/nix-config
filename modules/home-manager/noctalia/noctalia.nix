a14f1832ecbc79f8e667f90eefd979ef6ef6d232
Beat Scherrer <beat.scherrer@gmail.com>
2026-01-15 12:27:22 +0100

    update noctalia / niri

--- modules/home-manager/noctalia/noctalia.nix
+++ modules/home-manager/noctalia/noctalia.nix
@@ -4,93 +4,25 @@
   lib,
   ...
 }:
-with lib;
 let
   cfg = config.noctalia;
 in
 {
   options.noctalia = {
-    enable = mkEnableOption "noctalia";
-    barDensity = mkOption {
-      type = types.enum [
-        "compact"
-        "normal"
-      ];
-      default = "compact";
-      description = "Bar density setting";
-    };
-    barPosition = mkOption {
-      type = types.enum [
-        "top"
-        "bottom"
-        "left"
-        "right"
-      ];
-      default = "top";
-      description = "Bar position on screen";
-    };
-    colorScheme = mkOption {
-      type = types.str;
-      default = "monochrome";
-      description = "Color scheme for noctalia";
-    };
-    cornerRadius = mkOption {
-      type = types.float;
-      default = 0.2;
-      description = "Rounded corner ratio";
-    };
-    widgets = {
-      left = mkOption {
-        type = types.listOf types.str;
-        default = [
-          "sidepanel"
-          "wifi"
-          "bluetooth"
-        ];
-        description = "Left bar widgets";
-      };
-      center = mkOption {
-        type = types.listOf types.str;
-        default = [ "workspaces" ];
-        description = "Center bar widgets";
-      };
-      right = mkOption {
-        type = types.listOf types.str;
-        default = [
-          "battery"
-          "clock"
-        ];
-        description = "Right bar widgets";
-      };
-    };
+    enable = lib.mkEnableOption "noctalia";
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
       noctalia-shell
       brightnessctl
       gpu-screen-recorder
     ];
 
-    # Noctalia config directory
-    xdg.configFile."noctalia/settings.json".text = builtins.toJSON {
-      bar = {
-        density = cfg.barDensity;
-        position = cfg.barPosition;
-        widgets = {
-          left = cfg.widgets.left;
-          center = cfg.widgets.center;
-          right = cfg.widgets.right;
-        };
-      };
-      appearance = {
-        colorScheme = cfg.colorScheme;
-        roundedCornerRatio = cfg.cornerRadius;
-      };
-      clock = {
-        format = "HH:mm";
-        showSeconds = false;
-      };
-    };
+    # Out-of-store symlink so noctalia can modify settings dynamically
+    # while keeping the file version controlled in the repo
+    xdg.configFile."noctalia/settings.json".source =
+      config.lib.file.mkOutOfStoreSymlink
+        "${config.home.homeDirectory}/.nix/modules/home-manager/noctalia/settings.json";
   };
 }
