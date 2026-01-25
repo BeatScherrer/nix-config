6f74628e2dc5b78b80fd628dd1d5ae21f3efef7d
Beat Scherrer <beat.scherrer@gmail.com>
2026-01-25 16:02:04 +0100

    add lanzaboot config limit

--- hosts/trident/configuration.nix
+++ hosts/trident/configuration.nix
@@ -55,6 +55,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 50;
+  boot.lanzaboote.configurationLimit = 10;
   # boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
