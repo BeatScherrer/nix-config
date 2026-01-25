41de634325afe4e424a5d4aa9f6586bd28a4d37d
Beat Scherrer <beat.scherrer@gmail.com>
2026-01-25 16:03:24 +0100

    remove obsolete config limits

--- hosts/trident/configuration.nix
+++ hosts/trident/configuration.nix
@@ -54,8 +54,6 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 50;
-  boot.lanzaboote.configurationLimit = 10;
   # boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
