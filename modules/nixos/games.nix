e9d046c79b7cdf68298985e0ec07f9de0449300f
Beat Scherrer <beat.scherrer@gmail.com>
2026-01-28 22:55:34 +0100

    fix games

--- modules/nixos/games.nix
+++ modules/nixos/games.nix
@@ -1,13 +1,10 @@
 { pkgs, ... }:
 {
-  # FIXME:
-  # hardware.xpadneo.enable = true;
+  hardware.xpadneo.enable = true;
 
-  # services.hardware.xow.enable = true;
-
-  # boot.initrd.kernelModules = [
-  #   "usbhid"
-  #   "joydev"
-  #   "xpad"
-  # ];
+  boot.initrd.kernelModules = [
+    "usbhid"
+    "joydev"
+    "xpad"
+  ];
 }
