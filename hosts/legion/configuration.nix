ffc316080addb46bb2b1f17c38f7512ec1d8e1aa
Beat Scherrer <beat.scherrer@gmail.com>
2026-03-11 08:33:39 +0100

    use yubikey module in legion

--- hosts/legion/configuration.nix
+++ hosts/legion/configuration.nix
@@ -10,6 +10,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ../../modules/nixos/yubikey.nix
     ../../modules/nixos/default.nix
     ../../modules/nixos/nix.nix
     ../../modules/nixos/hardware/nvidia.nix
