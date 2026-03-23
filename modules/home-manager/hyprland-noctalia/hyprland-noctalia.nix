168d08af5448d18c8865cf9aabac95fcda5432c9
Beat Scherrer <beat.scherrer@gmail.com>
2026-03-23 17:08:50 +0100

    scratchpad from below

--- modules/home-manager/hyprland-noctalia/hyprland-noctalia.nix
+++ modules/home-manager/hyprland-noctalia/hyprland-noctalia.nix
@@ -112,6 +112,7 @@ in
             "borderangle, 1, 8, default"
             "fade, 1, 7, default"
             "workspaces, 1, 6, default"
+            "specialWorkspace, 1, 6, default, slidevert"
           ];
         };
 
