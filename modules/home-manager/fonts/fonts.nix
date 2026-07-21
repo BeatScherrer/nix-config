f462ce3e792decc25f6a855ddf652e07be9d6102
Beat Scherrer <beat.scherrer@gmail.com>
2026-07-21 11:47:07 +0200

    fix fonts module

--- modules/home-manager/fonts/fonts.nix
+++ modules/home-manager/fonts/fonts.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation {
 
   src = ./fonts;
 
-  buildInputs = lib.optional (!stdenv.isDarwin) [ fontconfig ];
+  buildInputs = lib.optionals (!stdenv.isDarwin) [ fontconfig ];
 
   installPhase =
     ''
